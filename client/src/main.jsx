47e8f1f06cb88f586977317b8c916c1031a82f2a
Luciano Vaccaro <luciano.vaccaro510@gmail.com>
2026-05-18 03:23:44 +0200

    fix: remove console log for Google client ID to clean up code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,6 @@ import "./App.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-console.log("CONTROLLO CLIENT ID CORRENTE:", clientId);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
